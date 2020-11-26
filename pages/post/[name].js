c52279a62ac2bbbaeba1262a9c61821a6ef3d8fc
ext-timothyl <timothy_lee@trendmicro.com>
2020-11-26 18:27:29 +0800

    feat: 🎸 Adjust header/post style

--- pages/post/[name].js
+++ pages/post/[name].js
@@ -2,15 +2,18 @@ import { getPost, getPosts } from "data";
 import Head from "next/head";
 import ReactMarkdown from "react-markdown";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import styles from "styles/Post.module.scss";
 import { tomorrow } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 const renderers = {
   // eslint-disable-next-line react/display-name
   code: ({ language, value }) => {
     return (
-      <SyntaxHighlighter style={tomorrow} language={language}>
-        {value}
-      </SyntaxHighlighter>
+      <p>
+        <SyntaxHighlighter style={tomorrow} language={language}>
+          {value}
+        </SyntaxHighlighter>
+      </p>
     );
   },
 };
@@ -19,14 +22,14 @@ export default function Post(props) {
   const { post } = props;
 
   return (
-    <>
+    <div className={styles.post}>
       <Head>
         <title>{post.metadata?.title}</title>
       </Head>
       <ReactMarkdown linkTarget="_blank" renderers={renderers}>
         {post.raw}
       </ReactMarkdown>
-    </>
+    </div>
   );
 }
 
