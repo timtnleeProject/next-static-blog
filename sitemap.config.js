7c85f1389c283b0c88a64aba5a226a4c90ce7119
ext-timothyl <timothy_lee@trendmicro.com>
2020-12-23 10:20:44 +0800

    fix: 🐛 Remove 404 from sitemap

--- sitemap.config.js
+++ sitemap.config.js
@@ -1,5 +1,18 @@
 module.exports = {
   siteUrl: "https://blog.timtnlee.me",
   generateRobotsTxt: true, // (optional)
-  // ...other options
+  transform: (config, url) => {
+    // custom function to ignore the url
+    if (/\/404/.test(url)) {
+      return null;
+    }
+
+    // Use default transformation for all other cases
+    return {
+      loc: url,
+      changefreq: config.changefreq,
+      priority: config.priority,
+      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+    };
+  },
 };
