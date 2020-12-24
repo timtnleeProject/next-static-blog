a97dece93f9cb9137d02c395039a03be7c71c3cf
ext-timothyl <timothy_lee@trendmicro.com>
2020-12-24 14:03:39 +0800

    feat: 🎸 Improve box shadow

--- components/Image.js
+++ components/Image.js
@@ -1,9 +1,13 @@
 import styles from "./styles/Image.module.scss";
+import classnames from "classnames";
 import { useEffect, useRef, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faSearch } from "@fortawesome/free-solid-svg-icons";
 
 export default function Image(props) {
   const { src, alt } = props;
   const [loaded, setLoaded] = useState(false);
+  const [canZoom, setCanZoom] = useState(false);
 
   const img = useRef();
   const zoomEl = useRef();
@@ -23,51 +27,63 @@ export default function Image(props) {
   }, []);
 
   useEffect(() => {
-    if (loaded) {
+    if (canZoom) {
       const imgEl = img.current;
-      const move = (e) => {
+      const inRange = (min, max) => (target) => target >= min && target <= max;
+      const createMove = (offsetX, offsetY) => (e) => {
         e.preventDefault();
         const event = e.touches?.[0] || e;
         const imgPos = img.current.getBoundingClientRect();
         const x = event.clientX - imgPos.x;
         const y = event.clientY - imgPos.y;
-        zoomEl.current.style.left = x + "px";
-        zoomEl.current.style.top = y + "px";
-        zoomImg.current.style.transform = `translate(-${(x / imgPos.width) * 100}%, -${
-          (y / imgPos.height) * 100
-        }%)`;
+        const percentX = (x / imgPos.width) * 100;
+        const percentY = (y / imgPos.height) * 100;
+
+        if (!inRange(-50, 150)(percentX) || !inRange(-50, 150)(percentY)) {
+          zoomEl.current.style.display = "none";
+        } else {
+          zoomEl.current.style.display = "block";
+        }
+        zoomEl.current.style.left = `${x}px`;
+        zoomEl.current.style.top = `${y}px`;
+        zoomEl.current.style.transform = `translate(-${offsetX}%, -${offsetY}%)`;
+        zoomImg.current.style.left = `${offsetX}%`;
+        zoomImg.current.style.top = `${offsetY}%`;
+        zoomImg.current.style.transform = `
+          translate(-${percentX}%, -${percentY}%)`;
       };
-      const enter = (e) => {
-        zoomEl.current.style.display = "block";
+      const mousemove = createMove(50, 50);
+      const touchmove = createMove(50, 90);
+      const enter = () => {
         zoomImg.current.style.width =
           img.current.getBoundingClientRect().width * 2 + "px";
-        move(e);
       };
-      const leave = (e) => {
+      const leave = () => {
         zoomEl.current.style.display = "none";
+        setCanZoom(false);
       };
-      imgEl.addEventListener("touchstart", enter);
-      imgEl.addEventListener("mouseenter", enter);
+      enter();
       imgEl.addEventListener("mouseleave", leave);
       imgEl.addEventListener("touchend", leave);
-      imgEl.addEventListener("mousemove", move);
-      imgEl.addEventListener("touchmove", move);
+      imgEl.addEventListener("mousemove", mousemove);
+      imgEl.addEventListener("touchmove", touchmove);
 
       return () => {
-        imgEl.removeEventListener("touchstart", enter);
-        imgEl.removeEventListener("mouseenter", enter);
         imgEl.removeEventListener("mouseleave", leave);
         imgEl.removeEventListener("touchend", leave);
-        imgEl.removeEventListener("mousemove", move);
-        imgEl.removeEventListener("touchmove", move);
+        imgEl.removeEventListener("mousemove", mousemove);
+        imgEl.removeEventListener("touchmove", touchmove);
       };
     }
-  }, [loaded]);
+  }, [canZoom]);
 
   return (
     <>
-      <span className={styles.imgWrap}>
+      <span className={classnames(styles.imgWrap, canZoom && styles.canZoom)}>
         <span className={styles.img}>
+          {canZoom && (
+            <span className={styles.hint}>move your finger/mouse over the image</span>
+          )}
           <img src={src} alt={alt} ref={img} />
           {loaded && (
             <span className={styles.zoomArea} ref={zoomEl}>
@@ -78,6 +94,16 @@ export default function Image(props) {
         </span>
         <span className={styles.info}>
           <small>{alt}</small>
+          {loaded &&
+            (canZoom ? (
+              <span className={styles.icon}>
+                <FontAwesomeIcon icon={faEye} />
+              </span>
+            ) : (
+              <span className={styles.icon} onClick={() => setCanZoom(true)}>
+                <FontAwesomeIcon icon={faSearch} />
+              </span>
+            ))}
         </span>
       </span>
     </>
