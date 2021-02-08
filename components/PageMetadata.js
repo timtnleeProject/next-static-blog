75f3eefb34435b6ce1fc3fd41d1211d771eeb857
ext-timothyl <timothy_lee@trendmicro.com>
2021-02-08 18:35:06 +0800

    fix: 🐛 remove duplicated ad script

--- components/PageMetadata.js
+++ components/PageMetadata.js
@@ -1,9 +1,19 @@
-import React, { memo } from "react";
+import React, { memo, useEffect } from "react";
 import PropTypes from "prop-types";
 import Head from "next/head";
 import { SITE } from "setting";
 function PageMetadata({ title, image, description, home = false }) {
   const fullTitle = home ? title : `${title} - ${SITE.title}`;
+  useEffect(() => {
+    const adScript = document.createElement("SCRIPT");
+    adScript.setAttribute("data-ad-client", "ca-pub-1331251306729236");
+    adScript.async = true;
+    adScript.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";
+    document.body.appendChild(adScript);
+    return () => {
+      document.body.removeChild(adScript);
+    };
+  }, []);
   return (
     <Head>
       <title>{fullTitle}</title>
@@ -11,11 +21,6 @@ function PageMetadata({ title, image, description, home = false }) {
       <meta name="description" content={description} />
       <meta property="og:description" content={description} />
       {image && <meta property="og:image" content={image} />}
-      <script
-        data-ad-client="ca-pub-1331251306729236"
-        async
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-      ></script>
     </Head>
   );
 }
