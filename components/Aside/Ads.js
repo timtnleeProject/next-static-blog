b84bed5e76b32c0038f8aef7dcdf8a92b0dc6554
ext-timothyl <timothy_lee@trendmicro.com>
2021-02-09 12:24:37 +0800

    feat: 🎸 test adSense: try to reload

--- components/Aside/Ads.js
+++ components/Aside/Ads.js
@@ -1,6 +1,6 @@
 import Card from "components/Card";
 import { useRouter } from "next/router";
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 
 const adScript = `
 <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
@@ -10,25 +10,16 @@ data-ad-format="fluid"
 data-ad-layout-key="-4m+bz-7b-d0+1ni"
 data-ad-client="ca-pub-1331251306729236"
 data-ad-slot="4673967329"></ins>
-<script>
-(adsbygoogle = window.adsbygoogle || []).push({});
-</script>`;
+`;
 
 export default function Ads() {
-  const ref = useRef();
   const router = useRouter();
 
   useEffect(() => {
-    const el = ref.current;
-    el.innerHTML = adScript;
-    return () => {
-      el.innerHTML = "";
-    };
+    (window.adsbygoogle || []).push({});
   }, [router.asPath]);
 
   return (
-    <Card className="g-mt-2 g-px-1">
-      <div ref={ref}></div>
-    </Card>
+    <Card className="g-mt-2 g-px-1" dangerouslySetInnerHTML={{ __html: adScript }}></Card>
   );
 }
