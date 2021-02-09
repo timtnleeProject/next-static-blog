6912c8693ca6e4d2d952a125398e3eec5e2ada66
ext-timothyl <timothy_lee@trendmicro.com>
2021-02-09 13:35:41 +0800

    feat: 🎸 test adSense: update ads style

--- components/Aside/Ads.js
+++ components/Aside/Ads.js
@@ -1,15 +1,14 @@
-import Card from "components/Card";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 
 const adScript = `
 <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 <ins class="adsbygoogle"
-style="display:block;width:100%;"
-data-ad-format="fluid"
-data-ad-layout-key="-4m+bz-7b-d0+1ni"
-data-ad-client="ca-pub-1331251306729236"
-data-ad-slot="4673967329"></ins>
+  style="display:block"
+  data-ad-format="fluid"
+  data-ad-layout-key="-4l+c0-7f-ep+1qt"
+  data-ad-client="ca-pub-1331251306729236"
+  data-ad-slot="4673967329"></ins>
 `;
 
 export default function Ads() {
@@ -19,7 +18,5 @@ export default function Ads() {
     (window.adsbygoogle || []).push({});
   }, [router.asPath]);
 
-  return (
-    <Card className="g-mt-2 g-px-1" dangerouslySetInnerHTML={{ __html: adScript }}></Card>
-  );
+  return <div dangerouslySetInnerHTML={{ __html: adScript }}></div>;
 }
