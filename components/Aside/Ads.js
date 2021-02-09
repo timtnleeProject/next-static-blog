2f5b8d4fffa1b58170e0701f06f59c616e87aede
ext-timothyl <timothy_lee@trendmicro.com>
2021-02-09 13:39:34 +0800

    feat: 🎸 test adSense

--- components/Aside/Ads.js
+++ components/Aside/Ads.js
@@ -4,19 +4,22 @@ import { useEffect } from "react";
 const adScript = `
 <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 <ins class="adsbygoogle"
-  style="display:block"
-  data-ad-format="fluid"
-  data-ad-layout-key="-4l+c0-7f-ep+1qt"
-  data-ad-client="ca-pub-1331251306729236"
-  data-ad-slot="4673967329"></ins>
+     style="display:block"
+     data-ad-format="fluid"
+     data-ad-layout-key="-4l+c0-7f-ep+1qt"
+     data-ad-client="ca-pub-1331251306729236"
+     data-ad-slot="4673967329"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
 `;
 
 export default function Ads() {
   const router = useRouter();
 
-  useEffect(() => {
-    (window.adsbygoogle || []).push({});
-  }, [router.asPath]);
+  // useEffect(() => {
+  //   (window.adsbygoogle || []).push({});
+  // }, [router.asPath]);
 
-  return <div dangerouslySetInnerHTML={{ __html: adScript }}></div>;
+  return <div className="g-my-1" dangerouslySetInnerHTML={{ __html: adScript }}></div>;
 }
