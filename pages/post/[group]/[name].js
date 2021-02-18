42cc4d6f0208f4e46fe77ddce80d72595e3db523
ext-timothyl <timothy_lee@trendmicro.com>
2021-02-18 11:36:32 +0800

    Test Ads

--- pages/post/[group]/[name].js
+++ pages/post/[group]/[name].js
@@ -153,12 +153,14 @@ export default function Post(props) {
         >
           {post.raw}
         </ReactMarkdown>
-        <Ads
-          data-ad-layout="in-article"
-          data-ad-format="fluid"
-          data-ad-client="ca-pub-1331251306729236"
-          data-ad-slot="3032107812"
-        />
+        <div>
+          <Ads
+            data-ad-layout="in-article"
+            data-ad-format="fluid"
+            data-ad-client="ca-pub-1331251306729236"
+            data-ad-slot="3032107812"
+          />
+        </div>
         <div className={styles.declare}>
           <div>
             <FontAwesomeIcon icon={faExclamationCircle} />
