e44206a4a7baf60a8131f311ba9cda91066a6127
ext-timothyl <timothy_lee@trendmicro.com>
2021-02-18 10:31:10 +0800

    Test Ads

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,17 @@ import Page from "components/Page";
 import PageMetadata from "components/PageMetadata";
 import { SITE } from "setting";
 import Ads from "components/Ads";
-import { Fragment } from "react";
 import styles from "styles/Home.module.scss";
+import { useMemo } from "react";
 
 export default function Home(props) {
   const { posts = [] } = props;
+  const postsWithAds = useMemo(() => {
+    return posts.reduce((ary, post, i) => {
+      if (i % 5 === 4) return ary.concat(["ads", post]);
+      return ary.concat([post]);
+    }, []);
+  }, [posts]);
   return (
     <Page.Content>
       <PageMetadata
@@ -19,19 +25,22 @@ export default function Home(props) {
       />
       <Page.Title>最近發表</Page.Title>
       <Post.VerticalList>
-        {posts.map((post, i) => (
-          <Post.VerticalItem key={i} post={post} />
-        ))}
+        {postsWithAds.map((post, i) =>
+          post === "ads" ? (
+            <div className={styles.slot}>
+              <Ads
+                style={{ display: "block" }}
+                data-ad-client="ca-pub-1331251306729236"
+                data-ad-slot="6140475128"
+                data-ad-format="auto"
+                data-full-width-responsive="true"
+              />
+            </div>
+          ) : (
+            <Post.VerticalItem key={i} post={post} />
+          ),
+        )}
       </Post.VerticalList>
-      <div className={styles.slot}>
-        <Ads
-          style={{ display: "block" }}
-          data-ad-client="ca-pub-1331251306729236"
-          data-ad-slot="6140475128"
-          data-ad-format="auto"
-          data-full-width-responsive="true"
-        />
-      </div>
     </Page.Content>
   );
 }
