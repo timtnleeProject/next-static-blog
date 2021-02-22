3bbe8c26f5b399a754798f497e0c457960a882c2
ext-timothyl <timothy_lee@trendmicro.com>
2021-02-22 18:01:58 +0800

    Test Ads

--- components/AdsTest.js
+++ components/AdsTest.js
@@ -1,17 +1,16 @@
 import React, { memo, useEffect, useRef } from "react";
 
-export default memo(function AdsTest() {
+export default memo(function Ads(props) {
   const content = `
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-    <ins class="adsbygoogle"
-         style="display:block; text-align:center;"
-         data-ad-layout="in-article"
-         data-ad-format="fluid"
-         data-ad-client="ca-pub-1331251306729236"
-         data-ad-slot="3032107812"></ins>
-    <script>
-         (adsbygoogle = window.adsbygoogle || []).push({});
-    </script>
-    `;
+  <ins class="adsbygoogle"
+       style="display:block; text-align:center;"
+       data-ad-layout="in-article"
+       data-ad-format="fluid"
+       data-ad-client="ca-pub-1331251306729236"
+       data-ad-slot="3032107812"></ins>
+  `;
+  useEffect(() => {
+    if (window) (window.adsbygoogle = window.adsbygoogle || []).push({});
+  }, []);
   return <div dangerouslySetInnerHTML={{ __html: content }}></div>;
 });
