bd884d01e9fd426bc7dddee911adc3951f3819e0
ext-timothyl <timothy_lee@trendmicro.com>
2021-02-22 17:33:26 +0800

    Test ads

--- components/Ads.js
+++ components/Ads.js
@@ -4,13 +4,5 @@ export default memo(function Ads(props) {
   useEffect(() => {
     (window.adsbygoogle || []).push({});
   }, []);
-  return (
-    <>
-      <script
-        async
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-      ></script>
-      <ins className="adsbygoogle" {...props}></ins>
-    </>
-  );
+  return <ins className="adsbygoogle" {...props}></ins>;
 });
