c9cd9a7f8e81b7e6cb2683adc4b0e26ed8955df3
ext-timothyl <timothy_lee@trendmicro.com>
2021-02-22 16:59:09 +0800

    Test perf

--- pages/post/[group]/[name].js
+++ pages/post/[group]/[name].js
@@ -158,13 +158,13 @@ export default function Post(props) {
             此文章出自<b>{SITE.title}</b>，請勿抄襲，轉載請註明出處。
           </div>
         </div>
-        <Ads
+        {/* <Ads
           style={{ display: "block", textAlign: "center" }}
           data-ad-layout="in-article"
           data-ad-format="fluid"
           data-ad-client="ca-pub-1331251306729236"
           data-ad-slot="3032107812"
-        />
+        /> */}
         <DiscussionEmbed
           shortname={DISQUS.shortname}
           config={{
