424e73f64e1dc1c718d39092859f9d45c4fc643c
ext-timothyl <timothy_lee@trendmicro.com>
2021-02-23 18:33:49 +0800

    Test Ads

--- pages/_app.js
+++ pages/_app.js
@@ -27,6 +27,7 @@ function MyApp({ Component, pageProps }) {
           <PostGroup />
           <NewPosts />
           <AdsFrame src="/ads/aside.html" />
+          <AdsFrame src="/ads/aside.html" />
         </App.Aside>
       </App.Body>
       <App.Footer />
