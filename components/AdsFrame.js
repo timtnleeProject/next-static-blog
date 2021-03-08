85806e37fa85e32c77299252dd0068bd4f73eadc
ext-timothyl <timothy_lee@trendmicro.com>
2021-03-08 14:39:50 +0800

    Test Ads

--- components/AdsFrame.js
+++ components/AdsFrame.js
@@ -4,20 +4,19 @@ import styles from "./styles/AdsFrame.module.scss";
 
 export default memo(function Ads(props) {
   return (
-    <div className={styles.ads}>
-      <iframe
-        className={classnames(styles.frame, props.className)}
-        src={props.src}
-        onLoad={(e) => {
-          const obj = e.target;
-          obj.style.height =
-            obj.contentWindow.document.documentElement.scrollHeight + "px";
-          obj.classList.add(styles.show);
-          obj.contentWindow.document.body.style = "overflow: hidden";
-        }}
-        scrolling="false"
-        frameBorder="0"
-      />
-    </div>
+    <iframe
+      className={classnames(styles.frame, props.className)}
+      src={props.src}
+      onLoad={(e) => {
+        const obj = e.target;
+        const h = obj.contentWindow.document.documentElement.scrollHeight;
+        obj.style.height = (h > 100 ? h : 0) + "px";
+        console.log("LOADED", h);
+        obj.classList.add(styles.show);
+        obj.contentWindow.document.body.style = "overflow: hidden";
+      }}
+      scrolling="false"
+      frameBorder="0"
+    />
   );
 });
